52c2d6ccb62faad5dc926712048b6b5043fd83ef
Juan Mosquera <juan.mosquera@unifyglobal.com>
2017-09-21 15:05:32 -0500

    add player list in tournament create

--- altorendimiento/resources/views/tournament/tournament_form.blade.php
+++ altorendimiento/resources/views/tournament/tournament_form.blade.php
@@ -4,6 +4,7 @@
 </div>
 
 <div class="form-group">
+	{!!  Form::label('season', 'Temporada') !!}
 	{!! Form::select('season_id',$seasons->pluck('name','id'), null, array('class' => 'form-control')) !!}
 </div>
 
@@ -16,3 +17,25 @@
     {!! Form::label('date_end', 'Fecha de Termino') !!}
     {!! Form::date('date_end', null, array('class' => 'form-control')) !!}
 </div>
+
+<table class="table table-striped table-bordered">
+    <thead>
+        <tr>
+            <td>Jugador</td>
+            <td></td>
+            <!-- <td><a class="btn btn-small btn-warning" href="{{ URL::to('players/create') }}">Crear</a></td> -->
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($players as $key => $value)
+        <tr>
+            <td>{{ $value->name }}</td>
+
+            <!-- we will also add show, edit, and delete buttons -->
+            <td> 
+				{{ Form::checkbox('players_id[]', $value->id) }} 
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
