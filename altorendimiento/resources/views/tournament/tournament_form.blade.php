addcdf77e5458a8dab17ffd024ff5e2c34ee7398
Juan Mosquera <juan.mosquera@unifyglobal.com>
2017-09-21 20:03:53 -0500

    fix

--- altorendimiento/resources/views/tournament/tournament_form.blade.php
+++ altorendimiento/resources/views/tournament/tournament_form.blade.php
@@ -36,8 +36,7 @@
 			<td>{!! Form::number('allPlayers['.$key.'][player_number]', null, array('class' => 'form-control')) !!}</td>
 
             <td> 
-                {!! Form::checkbox('allPlayers['.$key.'][is_checked]', true, in_array($player->id, $tournament->players)) !!} 
-            	
+                {!! Form::checkbox('allPlayers['.$key.'][is_checked]', true, in_array($player->id, $tournament->players)) !!}             	
             </td>
         </tr>
     @endforeach
