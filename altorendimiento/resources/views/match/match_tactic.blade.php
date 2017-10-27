ecf08aca202c77d1ee9fa03c1385f06aa705f057
Juan Mosquera <juan.mosquera@unifyglobal.com>
2017-10-26 19:10:37 -0500

    fix

--- altorendimiento/resources/views/match/match_tactic.blade.php
+++ altorendimiento/resources/views/match/match_tactic.blade.php
@@ -19,10 +19,7 @@
 		<div class="form-group">
 		    {!!  Form::label('tactical', 'Táctica', ['class' => 'control-label col-md-4']) !!}
 		    <div class="col-md-4">
-		    	{!! Form::select('tactical_id', $tacticals->pluck('name','id'), null , array('class' => 'form-control', 'placeholder' => 'Agregar Táctica', 'required' => '')) !!}
-		    </div>
-		    <div class="col-md-2">
-		    	{!! Form::submit('Agregar', array('class' => 'btn btn-primary')) !!}
+		    	{!! Form::select('tactical_id', $tacticals->pluck('name','id'), null , array('class' => 'form-control', 'placeholder' => 'Seleccionar Táctica', 'required' => '')) !!}
 		    </div>
 		</div>
 		<div class="form-group">
@@ -32,22 +29,40 @@
 			</div>
 		</div>
 		<div class="form-group">
-			{!!  Form::label('is_own', 'Culminó en gol', ['class' => 'control-label col-md-4']) !!}
+			{!!  Form::label('ended_in_goal', 'Culminó en gol', ['class' => 'control-label col-md-4']) !!}
 			<div class="col-md-1">
 				{!! Form::checkbox('ended_in_goal', 1) !!}
 			</div>
 		</div>
+		<div class="col-md-12 text-center">
+    		{!! Form::submit('Agregar', array('class' => 'btn btn-primary')) !!}  <br><br>
+        </div>		
 	{!! Form::close() !!}
 
-	{!! Form::model($match, ['route' => ['matchs.updateTechnicalPhysical', $match->id], 'method' => 'PUT', 'class' => 'form-horizontal']) !!}
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<h3>Tácticas</h3>
 
-	{!! Form::hidden('id', $match->id) !!}
-	{!! Form::hidden('tournament_id', $match->tournament_id) !!}
-	{!! Form::hidden('rival_team_id', $match->rival_team_id) !!}
-	{!! Form::hidden('stadium_id', $match->stadium_id) !!}
-
-	{!! Form::close() !!}
-
+			<table class="table table-striped table-bordered">
+			    <thead>
+			        <tr>
+			            <td>Táctica</td>
+			            <td>Es propia</td>
+			            <td>Culminó en gol</td>
+			        </tr>
+			    </thead>
+			    <tbody>
+			    	@foreach($match->tacticals as $key => $tactical)
+			    	<tr>
+			    		<td>{{ $tactical->name }}</td>
+			    		<td>{{ $tactical->pivot->is_own == 1 ? 'Sí' : 'No' }}</td>
+			    		<td>{{ $tactical->pivot->ended_in_goal == 1 ? 'Sí' : 'No' }}</td>
+			    	</tr>
+			    	@endforeach
+			    </tbody>
+			</table>
+		</div>
+	</div>
 </div>
 @stop
 
