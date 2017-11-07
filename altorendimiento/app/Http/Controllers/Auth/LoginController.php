e401720367bf472af945ec42b7700e718dba24b8
Josue Barrios <josue.barrios@unifyglobal.com>
2017-11-06 20:45:03 -0500

    Update

--- altorendimiento/app/Http/Controllers/Auth/LoginController.php
+++ altorendimiento/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +38,19 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    public function authenticated(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'role_id' =>2])) {
+            // Authentication passed...
+
+            return redirect()->intended($this->redirectPath());
+        }else{
+            return $this->sendFailedLoginResponse($request);
+
+        }
+
+
     }
 }
--- altorendimiento/routes/web.php
+++ altorendimiento/routes/web.php
@@ -17,36 +17,50 @@ Route::get('/', function () {
 
 Auth::routes();
 
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@authenticated');
+
 Route::get('/admin', 'Auth\AdminLoginController@showLoginForm');
 Route::post('/adminlogin', 'Auth\AdminLoginController@authenticated')->name('adminlogin');
 Route::post('/adminlogout', 'Auth\AdminLoginController@logout');
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
-Route::get('/adminhome', 'HomeController@admin')->name('home');
+Route::get('/adminhome', 'HomeController@admin')->name('home')->middleware('auth');
 
 
-Route::resource('seasons','SeasonController');
 
-Route::resource('players','PlayerController');
 
-Route::resource('tournaments','TournamentController');
+Route::group(['middleware' => 'auth'], function()
+{
+    Route::resource('seasons','SeasonController');
 
-Route::resource('rival_teams','Rival_TeamController');
+    Route::resource('players','PlayerController');
 
-Route::resource('stadiums','StadiumController');
+    Route::resource('tournaments','TournamentController');
 
-Route::resource('matchs','MatchController');
-Route::get('/matchs/{id}/editTechnicalPhysical', 'MatchController@editTechnicalPhysical')->name('matchs.editTechnicalPhysical');
-Route::put('/matchs/{id}/updateTechnicalPhysical', 'MatchController@updateTechnicalPhysical')->name('matchs.updateTechnicalPhysical');
+    Route::resource('rival_teams','Rival_TeamController');
 
-Route::get('/matchs/{id}/editTactical', 'MatchController@editTactical')->name('matchs.editTactical');
-Route::put('/matchs/{id}/updateTactical', 'MatchController@updateTactical')->name('matchs.updateTactical');
-Route::put('/matchs/{id}/createTactical', 'MatchController@createTactical')->name('matchs.createTactical');
+    Route::resource('stadiums','StadiumController');
 
-Route::resource('tacticals','TacticalController');
+    Route::resource('tacticals','TacticalController');
 
-Route::resource('clubs','ClubController');
+    Route::resource('clubs','ClubController');
 
-Route::resource('administrators','UserController');
+    Route::resource('administrators','UserController');
+
+    Route::resource('matchs','MatchController');
+
+});
+
+
+
+Route::get('/matchs/{id}/editTechnicalPhysical', 'MatchController@editTechnicalPhysical')->name('matchs.editTechnicalPhysical')->middleware('auth');
+Route::put('/matchs/{id}/updateTechnicalPhysical', 'MatchController@updateTechnicalPhysical')->name('matchs.updateTechnicalPhysical')->middleware('auth');
+
+Route::get('/matchs/{id}/editTactical', 'MatchController@editTactical')->name('matchs.editTactical')->middleware('auth');
+Route::put('/matchs/{id}/updateTactical', 'MatchController@updateTactical')->name('matchs.updateTactical')->middleware('auth');
+Route::put('/matchs/{id}/createTactical', 'MatchController@createTactical')->name('matchs.createTactical')->middleware('auth');
+
+
 
